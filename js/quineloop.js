e1ea309a2ac4a3c1afc68a1484052a15e8df6a82
gautamc <gautam.chekuri@gmail.com>
2013-07-30 02:35:46 -0400

    rendering a mandelbrot set

--- js/quineloop.js
+++ js/quineloop.js
@@ -63,16 +63,33 @@ var quineloop = (function() {
         var draw = function(){
             var img_data = ctx.createImageData(1, 1)
             var pixel_data = img_data.data
-            for(var x = 0; x < me.width; x++) {
-                for(var y = 0; y < 100; y++) {
-                    pixel_data[0] = (Math.random()*1000)%255
-                    pixel_data[1] = (Math.random()*1000)%255
-                    pixel_data[2] = (Math.random()*1000)%255
-                    pixel_data[3] = (Math.random()*1000)%255
-                    ctx.putImageData(img_data, x, y)
+            var real_scale = d3.scale.linear()
+                .domain([0,me.width])
+                .range([-2.0, 1])
+            var imgr_scale = d3.scale.linear()
+                .domain([0,me.height])
+                .range([1, -1])
+
+            for(var i = 0; i < me.width; i++) {
+                for(var j = 0; j < me.height; j++) {
+
+                    var x_0 = real_scale(i), y_0 = imgr_scale(j), x = 0, y = 0, itr = 0, max_itr = 1000
+                    while( ((x*x)+(y*y) < 4) && (itr < max_itr) ){
+                        var x_temp = ((x*x) - (y*y)) + x_0
+                        y = (2 * x * y) + y_0
+                        x = x_temp
+                        itr = itr + 1
+                    }
+                    if( itr == max_itr ) {
+                        pixel_data[0] = 0
+                        pixel_data[1] = 0
+                        pixel_data[2] = 0
+                        pixel_data[3] = 255
+                        ctx.putImageData(img_data, i, j)
+                    } else {
+                    }
                 }
             }
-            
         }
 
         me.reset = function(){
