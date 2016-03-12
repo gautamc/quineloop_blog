b592b8584635e402ea055331836e2fbe948c87f8
gautamc <gautam.chekuri@gmail.com>
2016-03-12 17:31:58 -0500

    updates homepage

--- js/mandelbrot.js
+++ js/mandelbrot.js
@@ -1,87 +1,95 @@
 var quineloop = (function() {
 
-    var Mandelbrot = function(args){
-        var i = 0, canvas, ctx, me = this
+  var Mandelbrot = function(args) {
+    var i = 0, canvas, ctx, me = this
 
-        var init = function(args){
-            me.args = args
-            me.width = $("#"+args.eid).width() / 2 //$("#"+args.eid).width()
-            me.height =  me.width / 1.7
+    var init = function(args) {
+      me.args = args
+      me.width = $("#"+args.eid).width() / 2 //$("#"+args.eid).width()
+      me.height =  me.width / 1.7
 
-            canvas = document.getElementById(args.eid).appendChild(
-                document.createElement('canvas')
-            )
-            canvas.setAttribute("class", "mandelbrot-canvas")
-            canvas.setAttribute("height", me.height)
-            canvas.setAttribute("width", me.width)
-            ctx = canvas.getContext('2d')
+      canvas = document.getElementById(args.eid).appendChild(
+        document.createElement('canvas')
+      )
+      canvas.setAttribute("class", "mandelbrot-canvas")
+      canvas.setAttribute("height", me.height)
+      canvas.setAttribute("width", me.width)
+      ctx = canvas.getContext('2d')
 
-            draw()
-        }
-
-        var draw = function(){
-            var img_data = ctx.createImageData(1, 1)
-            var pixel_data = img_data.data
-            var real_scale = d3.scale.linear()
-                .domain([0,me.width])
-                .range([-2.0, 1])
-            var imgr_scale = d3.scale.linear()
-                .domain([0,me.height])
-                .range([1, -1])
-            var color_scale = d3.scale.category20()
-                .domain([1, 1000])
-
-            var render_segment = function(init_idx, segment_width, full_width){
-                var end_idx = init_idx + segment_width > full_width ? full_width
-                    : init_idx + segment_width
-                for(var i = init_idx; i < end_idx; i++) {
-                    for(var j = 0; j < me.height; j++) {
-
-                        var x_0 = real_scale(i), y_0 = imgr_scale(j),
-                            x = 0, y = 0, itr = 0, max_itr = 1000
-
-                        while( ((x*x)+(y*y) < 4) && (itr < max_itr) ){
-                            var x_temp = ((x*x) - (y*y)) + x_0
-                            y = (2 * x * y) + y_0
-                            x = x_temp
-                            itr = itr + 1
-                        }
-                        if( itr == max_itr ) {
-                            pixel_data[0] = 0
-                            pixel_data[1] = 0
-                            pixel_data[2] = 0
-                            pixel_data[3] = 255
-                            ctx.putImageData(img_data, i, j)
-                        } else {
-                            var non_blk_color = d3.rgb( color_scale(itr) )
-                            pixel_data[0] = non_blk_color.r
-                            pixel_data[1] = non_blk_color.g
-                            pixel_data[2] = non_blk_color.b
-                            pixel_data[3] = 255
-                            ctx.putImageData(img_data, i, j)
-                        }
-                    }
-                }
-
-                if(end_idx == full_width) {
-                    $("div#rendering-mandelbrot").hide()
-                } else {
-                    window.setTimeout( render_segment(end_idx, segment_width, full_width), 0 )
-                }
-            }
-            window.setTimeout( render_segment(0, Math.floor(me.width / 3), me.width), 3000 )
-        }
-
-        me.reset = function(){
-        }
-
-        init(args)
-        return me
+      draw()
     }
 
-    return { Mandelbrot: Mandelbrot }
+    var draw = function() {
+      var img_data = ctx.createImageData(1, 1)
+      var pixel_data = img_data.data
+      var real_scale = d3.scale.linear()
+        .domain([0,me.width])
+        .range([-2.0, 1])
+      var imgr_scale = d3.scale.linear()
+        .domain([0,me.height])
+        .range([1, -1])
+      var color_scale = d3.scale.category20()
+        .domain([1, 1000])
+
+      var render_segment = function(init_idx, segment_width, full_width){
+        var end_idx = init_idx + segment_width > full_width ? full_width
+          : init_idx + segment_width
+        for(var i = init_idx; i < end_idx; i++) {
+          for(var j = 0; j < me.height; j++) {
+
+            var x_0 = real_scale(i), y_0 = imgr_scale(j),
+            x = 0, y = 0, itr = 0, max_itr = 1000
+
+            while( ((x*x)+(y*y) < 4) && (itr < max_itr) ){
+              var x_temp = ((x*x) - (y*y)) + x_0
+              y = (2 * x * y) + y_0
+              x = x_temp
+              itr = itr + 1
+            }
+            if( itr == max_itr ) {
+              pixel_data[0] = 0
+              pixel_data[1] = 0
+              pixel_data[2] = 0
+              pixel_data[3] = 255
+              ctx.putImageData(img_data, i, j)
+            } else {
+              var non_blk_color = d3.rgb( color_scale(itr) )
+              pixel_data[0] = non_blk_color.r
+              pixel_data[1] = non_blk_color.g
+              pixel_data[2] = non_blk_color.b
+              pixel_data[3] = 255
+              ctx.putImageData(img_data, i, j)
+            }
+          }
+        }
+
+        if(end_idx == full_width) {
+          $("div#rendering-mandelbrot").hide()
+        } else {
+          window.setTimeout( render_segment(end_idx, segment_width, full_width), 0 )
+        }
+      }
+      window.setTimeout( render_segment(0, Math.floor(me.width / 3), me.width), 3000 )
+    }
+
+    me.reset = function() {
+    }
+
+    init(args)
+    return me
+  }
+
+  return { Mandelbrot: Mandelbrot }
 })()
 
-$(window).load(function(){
-    var mandelbrot = new quineloop.Mandelbrot({ eid: "mandelbrot" })
-})
+$(document).ready(
+  function(){
+    window.setTimeout(
+      function() {
+        var mandelbrot = new quineloop.Mandelbrot({ eid: "mandelbrot" })
+      },
+      1000
+    )
+  }
+)
+
--- js/quineloop.js
+++ js/quineloop.js
@@ -1,110 +1,179 @@
 var quineloop = (function() {
 
-    var DeadChannel = function(args){
-        var i = 0, canvas, ctx, me = this
+  var DeadChannel = function(args){
+    var i = 0, canvas, ctx, me = this
 
-        var init = function(args){
-            me.args = args
-            me.width = $("#"+args.eid).width()
-            me.height = 100
+    var init = function(args){
+      me.args = args
+      me.width = $("#"+args.eid).width()
+      me.height = 100
 
-            canvas = document.getElementById(args.eid).appendChild(
-                document.createElement('canvas')
-            )
-            canvas.setAttribute("class", "dead-channel-canvas")
-            canvas.setAttribute("height", me.height)
-            canvas.setAttribute("width", me.width)
-            ctx = canvas.getContext('2d')
+      canvas = document.getElementById(args.eid).appendChild(
+        document.createElement('canvas')
+      )
+      canvas.setAttribute("class", "dead-channel-canvas")
+      canvas.setAttribute("height", me.height)
+      canvas.setAttribute("width", me.width)
+      ctx = canvas.getContext('2d')
 
-            draw()
-        }
-
-        var draw = function(){
-            var img_data = ctx.createImageData(1, 1)
-            var pixel_data = img_data.data
-            for(var x = 0; x < me.width; x++) {
-                for(var y = 0; y < 100; y++) {
-                    pixel_data[0] = (Math.random()*1000)%255
-                    pixel_data[1] = (Math.random()*1000)%255
-                    pixel_data[2] = (Math.random()*1000)%255
-                    pixel_data[3] = (Math.random()*1000)%255
-                    ctx.putImageData(img_data, x, y)
-                }
-            }
-            $("div#rendering-dead-channel").hide()
-        }
-
-        me.reset = function(){
-        }
-
-        init(args)
-        return me
+      draw()
     }
 
-    var Mandelbrot = function(args){
-        var i = 0, canvas, ctx, me = this
-
-        var init = function(args){
-            me.args = args
-            me.width = $("#"+args.eid).width()
-            me.height = me.width / 1.7
-
-            canvas = document.getElementById(args.eid).appendChild(
-                document.createElement('canvas')
-            )
-            canvas.setAttribute("class", "mandelbrot-canvas")
-            canvas.setAttribute("height", me.height)
-            canvas.setAttribute("width", me.width)
-            ctx = canvas.getContext('2d')
-
-            draw()
+    var draw = function(){
+      var img_data = ctx.createImageData(1, 1)
+      var pixel_data = img_data.data
+      for(var x = 0; x < me.width; x++) {
+        for(var y = 0; y < 100; y++) {
+          pixel_data[0] = (Math.random()*1000)%255
+          pixel_data[1] = (Math.random()*1000)%255
+          pixel_data[2] = (Math.random()*1000)%255
+          pixel_data[3] = (Math.random()*1000)%255
+          ctx.putImageData(img_data, x, y)
         }
-
-        var draw = function(){
-            var img_data = ctx.createImageData(1, 1)
-            var pixel_data = img_data.data
-            var real_scale = d3.scale.linear()
-                .domain([0,me.width])
-                .range([-2.0, 1])
-            var imgr_scale = d3.scale.linear()
-                .domain([0,me.height])
-                .range([1, -1])
-
-            for(var i = 0; i < me.width; i++) {
-                for(var j = 0; j < me.height; j++) {
-
-                    var x_0 = real_scale(i), y_0 = imgr_scale(j), x = 0, y = 0, itr = 0, max_itr = 1000
-                    while( ((x*x)+(y*y) < 4) && (itr < max_itr) ){
-                        var x_temp = ((x*x) - (y*y)) + x_0
-                        y = (2 * x * y) + y_0
-                        x = x_temp
-                        itr = itr + 1
-                    }
-                    if( itr == max_itr ) {
-                        pixel_data[0] = 0
-                        pixel_data[1] = 0
-                        pixel_data[2] = 0
-                        pixel_data[3] = 255
-                        ctx.putImageData(img_data, i, j)
-                    } else {
-                    }
-                }
-            }
-            $("div#rendering-mandelbrot").hide()
-        }
-
-        me.reset = function(){
-        }
-
-        init(args)
-        return me
+      }
+      $("div#rendering-dead-channel").hide()
     }
 
+    me.reset = function(){
+    }
 
-    return { DeadChannel: DeadChannel, Mandelbrot: Mandelbrot }
+    init(args)
+    return me
+  }
+
+  var Mandelbrot = function(args){
+    var i = 0, canvas, ctx, me = this
+
+    var init = function(args){
+      me.args = args
+      me.width = $("#"+args.eid).width()
+      me.height = me.width / 1.7
+
+      canvas = document.getElementById(args.eid).appendChild(
+        document.createElement('canvas')
+      )
+      canvas.setAttribute("class", "mandelbrot-canvas")
+      canvas.setAttribute("height", me.height)
+      canvas.setAttribute("width", me.width)
+      ctx = canvas.getContext('2d')
+
+      draw()
+    }
+
+    var draw = function(){
+      var img_data = ctx.createImageData(1, 1)
+      var pixel_data = img_data.data
+      var real_scale = d3.scale.linear()
+        .domain([0,me.width])
+        .range([-2.0, 1])
+      var imgr_scale = d3.scale.linear()
+        .domain([0,me.height])
+        .range([1, -1])
+
+      for(var i = 0; i < me.width; i++) {
+        for(var j = 0; j < me.height; j++) {
+
+          var x_0 = real_scale(i), y_0 = imgr_scale(j), x = 0, y = 0, itr = 0, max_itr = 1000
+          while( ((x*x)+(y*y) < 4) && (itr < max_itr) ){
+            var x_temp = ((x*x) - (y*y)) + x_0
+            y = (2 * x * y) + y_0
+            x = x_temp
+            itr = itr + 1
+          }
+          if( itr == max_itr ) {
+            pixel_data[0] = 0
+            pixel_data[1] = 0
+            pixel_data[2] = 0
+            pixel_data[3] = 255
+            ctx.putImageData(img_data, i, j)
+          } else {
+          }
+        }
+      }
+      $("div#rendering-mandelbrot").hide()
+    }
+
+    me.reset = function(){
+    }
+
+    init(args)
+    return me
+  }
+
+  var Lissajous = function(args) {
+    var canvas, ctx, me = this
+
+    var init = function(args) {
+      me.args = args
+      me.width = $("#"+args.eid).width()
+      me.height = me.width / 1.7
+      me.curves = []
+
+      canvas = document.getElementById(args.eid).appendChild(
+        document.createElement('canvas')
+      )
+      canvas.setAttribute("class", "lissajous-canvas")
+      canvas.setAttribute("height", me.height)
+      canvas.setAttribute("width", me.width)
+      ctx = canvas.getContext('2d')
+
+      for(i = 0; i < 10; i++) {
+        me.curves.push(
+          new Curve(ctx, me)
+        )
+      }
+
+      draw()
+      $("div#rendering-lissajous").hide()
+    }
+
+    var draw = function() {
+      ctx.clearRect(0, 0, me.width, me.height)
+      for(var i = 0; i < me.curves.length; i += 1) {
+	me.curves[i].update()
+      }
+      requestAnimationFrame(draw)
+    }
+
+    var Curve = function(ctx, args) {
+      var me = this
+      var setup = function() {
+        me.x_angle = Math.PI * 2 * Math.random()
+	me.y_angle = Math.PI * 2 * Math.random()
+	me.x_speed = -0.2 * Math.random()
+	me.y_speed = -0.2 * Math.random()
+	me.radius = 50 + Math.random() * 50
+      }
+
+      me.update = function() {
+	var x = Math.cos(me.x_angle) * me.radius,
+	    y = Math.sin(me.y_angle) * me.radius
+	me.x_angle += me.x_speed
+	me.y_angle += me.y_speed
+
+	ctx.beginPath()
+        ctx.rect(
+          args.width / 2 + x,
+          args.height / 2 + y,
+          6,
+          4
+        )
+	ctx.fill()
+      }
+
+      setup()
+      return me
+    }
+
+    init(args)
+    return me
+  }
+
+  return { DeadChannel: DeadChannel, Mandelbrot: Mandelbrot, Lissajous: Lissajous }
 })()
 
 $(window).load(function(){
-    var dead_channel = new quineloop.DeadChannel({ eid: "dead-channel" })
-    var mandelbrot = new quineloop.Mandelbrot({ eid: "mandelbrot" })
+  var dead_channel = new quineloop.DeadChannel({ eid: "dead-channel" })
+  var mandelbrot = new quineloop.Mandelbrot({ eid: "mandelbrot" })
+  var lissajous = new quineloop.Lissajous({ eid: "lissajous" })
 })
